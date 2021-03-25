9893e9e463aad82561a3d81bf09f622cd5f893a9
Bill Xiong <xiong.bill@yahoo.com>
2021-03-24 21:41:51 -0700

    MVP progress

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,84 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const express = require("express");
+const account = require("./accounts-model");
+const {
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId,
+} = require("./accounts-middleware");
 
-router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+const root = process.env.API_ROOT || `/api`;
+// DO YOUR MAGIC
+router.get(`${root}/accounts`, async (req, res, next) => {
+  try {
+    const data = await accounts.getAll();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get(`${root}/accounts/:id`, checkAccountId(), async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const data = await accounts.getById(req.params.id);
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post(
+  `${root}/accounts`,
+  checkAccountPayload(),
+  checkAccountNameUnique(),
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      const data = await accounts.create(req.body);
+      res.status(201).json(data);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  `${root}/accounts/:id`,
+  checkAccountId(),
+  checkAccountPayload(),
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      const data = await accounts.updateById(req.params.id, req.body);
+      res.status(200).json(data);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  `${root}/accounts/:id`,
+  checkAccountId(),
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      await accounts.deleteById(req.params.id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
   res.status(500).json({
-    message: 'something went wrong inside the accounts router',
+    message: "something went wrong inside the accounts router",
     errMessage: err.message,
-  })
-})
+  });
+});
 
 module.exports = router;
